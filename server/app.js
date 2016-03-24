d2885e37eae6ecb192a756f8db8b716dd073f871
lanmingming <lanmingming@baidu.com>
2016-03-24 14:25:41 +0800

    server setup in cli

--- server/app.js
+++ server/app.js
@@ -10,31 +10,26 @@ var router = require('./router/router');
 var List = require('./model/list');
 
 
-var setUpServer = function (port) {
 
-	var app = module.exports = koa();
-	render(app, configs.view)
+var app = module.exports = koa();
+render(app, configs.view)
 
-	// app.use(require('koa-bodyparser')(configs.bodyparser));
-	app.use(koaBody(configs.koaBody));
-	app.use(koaStatic(configs.static.directory, {}));
+// app.use(require('koa-bodyparser')(configs.bodyparser));
+app.use(koaBody(configs.koaBody));
+app.use(koaStatic(configs.static.directory, {}));
 
-	app.context.models = {};
-	app.context.models.lists = new List;
-
-
-	app.use(router())
+app.context.models = {};
+app.context.models.lists = new List;
 
 
-	if (!module.parent) {
-		app.listen(port || configs.port, function () {
-			console.log('Server runnin on port: ' + port || configs.port);
-		})
-	}
-	else {
-		module.exports = app;
-	}
-};
+app.use(router());
 
-models.exports = setUpServer;
 
+if (!module.parent) {
+	app.listen(configs.port, function () {
+		console.log('Server runnin on port: ' + configs.port);
+	})
+}
+else {
+	module.exports = app;
+}
