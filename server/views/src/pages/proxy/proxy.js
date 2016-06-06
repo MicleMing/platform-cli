0543ea7764b70a450351d25c69660b4fff26a06e
lanmingming <lanmingming@baidu.com>
2016-06-06 14:56:49 +0800

    proxy

--- server/views/src/pages/proxy/proxy.js
+++ server/views/src/pages/proxy/proxy.js
@@ -1,9 +1,11 @@
-$(function () {
-	var addRule = $('.proxy .proxy-wrap .add-rule');
-	var ruleList = $('.proxy .proxy-wrap .rule-list')
 
-	var tempNode = $('.proxy .proxy-list').children(':first').clone();
-	addRule.on('click', function (e) {
-		ruleList.append(tempNode);
-	});
+define(function (require, exports, module) {
+    var $ = require('lib/js/jquery');
+        var addRule = $('.proxy .proxy-wrap .add-rule');
+    var ruleList = $('.proxy .proxy-wrap .rule-list')
+
+    var tempNode = $('.proxy .proxy-list').children(':first').clone();
+    addRule.on('click', function (e) {
+        ruleList.append(tempNode);
+    });
 });
